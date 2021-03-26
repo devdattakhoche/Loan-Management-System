e66866519b69057a17ea92b199bf20a18cab0f4e
devdattakhoche <dkhoche70@gmail.com>
2021-03-26 20:04:49 +0530

    🎨 Flask App setup

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
