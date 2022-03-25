26aa6bead262f3720da1005c228e92d9f769fe11
Devdatta Khoche <49261633+devdattakhoche@users.noreply.github.com>
2022-03-25 08:08:33 +0530

    Docker fixes

--- project/app.py
+++ project/app.py
@@ -1,18 +1,16 @@
-
 from functools import wraps
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import jwt
 import os
+
 from project.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-from project.models import Users
-from project.constants import USERTYPE
 
 def get_key(val, dict):
     for key, value in dict.items():
@@ -24,16 +22,20 @@ def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
+        if "x-access-token" in request.headers:
+            token = request.headers["x-access-token"]
         if not token:
-            return jsonify({'message': 'Token is missing!'}), 401
+            return jsonify({"message": "Token is missing!"}), 401
         try:
-            data = jwt.decode(
-                token, app.config['SECRET_KEY'], algorithms=['HS256'])
-            loggedInUser = Users.query.filter_by(
-                public_id=data['public_id']).first()
+            data = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])
+            from project.models import Users
+
+            loggedInUser = Users.query.filter_by(public_id=data["public_id"]).first()
         except Exception as ex:
-            return jsonify({'message': 'Token is invalid!'}), 401
+            return jsonify({"message": "Token is invalid!"}), 401
         return f(loggedInUser, *args, **kwargs)
+
     return decorated
+
+
+import project.routes
