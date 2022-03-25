7b7fd298bb6747e6635a58e05cc1f0c62e569945
Devdatta Khoche <49261633+devdattakhoche@users.noreply.github.com>
2022-03-25 08:13:45 +0530

    Update docker_script.sh

--- project/docker/docker_script.sh
+++ project/docker/docker_script.sh
@@ -5,4 +5,4 @@ python manage.py db init
 python manage.py recreate_db
 python manage.py db migrate
 python manage.py db upgrade
-python manage.py runserver
+python manage.py runserver -h 0.0.0.0 -p 5000 -d
